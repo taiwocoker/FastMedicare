415b947ad3c15bef5d1a8ca1f4d5d519422c86fe
taiwocoker <taiwofcoker@gmail.com>
2021-08-08 22:39:47 +0100

    delete files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,7 +43,6 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
-  config.include FactoryBot::Syntax::Methods
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
@@ -93,10 +92,5 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
+
 end
